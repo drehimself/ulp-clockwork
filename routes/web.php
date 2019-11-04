9f2591a2d3a672ef85ae61c144e609f6eaf2a8b8
Andre Madarang <andre_madarang@hotmail.com>
2019-11-04 12:44:54 -0500

    working

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\User;
+use App\Mail\OrderShipped;
+use Illuminate\Support\Facades\Mail;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,5 +16,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    clock()->startEvent('get-users', "Get all users in database");
+    $users = User::all();
+    clock()->endEvent('get-users');
 });
+
+Route::get('/posts', 'PostController@index')->name('post.index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
